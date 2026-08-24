1d2ab4bbe76d27d91a339eca408489b1fdd2f857
AnhLDP <leducphananh@gmail.com>
2026-08-24 23:11:35 +0700

    feat: add categories and suppliers screens to app layout

--- src/app/(app)/_layout.tsx
+++ src/app/(app)/_layout.tsx
@@ -4,6 +4,8 @@ export default function AppLayout() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="categories" options={{ headerShown: false }} />
+      <Stack.Screen name="suppliers" options={{ headerShown: false }} />
     </Stack>
   );
 }
